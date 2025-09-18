dd904d56c4c662677f129e7687e924b26d325fcc
amisuu <liver13@wp.pl>
2025-09-18 20:00:43 +0200

    text correction.2

--- src/app/home-component/home-component.ts
+++ src/app/home-component/home-component.ts
@@ -8,7 +8,7 @@ import { setNonBreakingSpace } from '../shared/helpers';
   styleUrl: './home-component.scss'
 })
 export class HomeComponent {
-  contentText = '<strong>Nazywam się Natalia Sienkiewicz-Ciszewska.</strong> Jestem logopedą i neurologopedą. Ukończyłam studia na Uniwersytecie Gdańskim oraz Gdańskim Uniwersytecie Medycznym.<br> Specjalizuję się w terapii dzieci i młodzieży z trudnościami w zakresie rozwoju mowy, komunikacji oraz funkcji prymarnych, takich jak oddychanie, połykanie, gryzienie, żucie, pozycja spoczynkowa języka i warg.<br>Prowadzę również terapię miofunkcjonalną osób dorosłych, zwłaszcza przygotowujących się do leczenia ortodontycznego lub będących w jego trakcie. Terapia ta skupia się na pracy nad właściwym napięciem mięśniowym w jamie ustnej oraz podstawowymi funkcjami narządu orofacjalnego.';
+  contentText = '<strong>Nazywam się Natalia Sienkiewicz-Ciszewska.</strong> Jestem logopedą i neurologopedą. Ukończyłam studia na Uniwersytecie Gdańskim oraz Gdańskim Uniwersytecie Medycznym.</br></br>Specjalizuję się w terapii dzieci i młodzieży z trudnościami w zakresie rozwoju mowy, komunikacji oraz funkcji prymarnych, takich jak oddychanie, połykanie, gryzienie, żucie, pozycja spoczynkowa języka i warg.</br></br>Prowadzę również terapię miofunkcjonalną osób dorosłych, zwłaszcza przygotowujących się do leczenia ortodontycznego lub będących w jego trakcie. Terapia ta skupia się na pracy nad właściwym napięciem mięśniowym w jamie ustnej oraz podstawowymi funkcjami narządu orofacjalnego.';
 
   get nonBreakingSpace() {
     return setNonBreakingSpace(this.contentText);
